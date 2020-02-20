526b85ca72a920050cb0c0f0c051e830082bc18c
梦幻 <web2018_lang@163.com>
2020-02-21 00:36:58 +0800

    酒店首页

---
+++ components/hotel/hotelSelector.vue
@@ -0,0 +1,55 @@
+<template>
+  <sction class="selector">
+    <el-row>
+      <el-col :span="6">
+        <div class="grid-content bg-purple">
+          <p>
+            价格：
+            <span>0-4000</span>
+          </p>
+          <el-slider v-model="value1"></el-slider>
+        </div>
+      </el-col>
+      <el-col :span="4">
+        <div class="grid-content bg-purple-light">
+          <p>住宿等级</p>
+        </div>
+      </el-col>
+      <el-col :span="4">
+        <div class="grid-content bg-purple">
+          <p>住宿类型</p>
+        </div>
+      </el-col>
+      <el-col :span="4">
+        <div class="grid-content bg-purple-light">
+          <p>酒店设施</p>
+        </div>
+      </el-col>
+      <el-col :span="4">
+        <div class="grid-content bg-purple">
+          <p>酒店品牌</p>
+        </div>
+      </el-col>
+    </el-row>
+  </sction>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped lang="less">
+.el-row{
+    padding:10px;
+    margin-top:20px;
+    border:2px solid #DCDFE6;
+    .el-col{
+        height: 100%;
+        border-right:2px solid #DCDFE6;
+        padding-left:5px;
+
+    }
+}
+</style>>
--- pages/hotel/index.vue
+++ pages/hotel/index.vue
@@ -1,13 +1,63 @@
 <template>
-  <div>酒店首页</div>
+  <section class="container">
+    <!-- 顶部筛选器 -->
+    <HotelFlights/>
+
+    <!-- 区域信息 -->
+    <el-row type="flex">
+      <el-col :span="14">
+        <p>区域： 人民广场  顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶   顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶  顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶  顶顶顶顶顶顶顶顶顶顶顶</p>
+        <p>均价：@@@ ￥322  </p>
+      </el-col>
+      <el-col :span="10">
+        <!-- 地图组件 -->
+        <HotelMap/>
+      </el-col>
+    </el-row>
+
+    <!-- 选择器 -->
+    <HotelSelector/>
+
+    <el-row>
+      <el-col :span="8">
+        图片
+      </el-col>
+      <el-col :span="8">
+        <h2>如家快捷酒店</h2>
+        <p>ju jia juai jie hotel(fang zhou yan dian ) @@@ 经济型</p>
+        <p> *****   36条评价  46篇游记</p>
+        <p>位于：用法付付付付付付</p>
+      </el-col>
+      <el-col :span="8">
+        <div>携程 ￥<span>140</span>起></div>
+        <div>携程 ￥<span>140</span>起></div>
+        <div>携程 ￥<span>140</span>起></div>
+      </el-col>
+    </el-row>
+  </section>
 </template>
 
 <script>
+import HotelFlights from "@/components/hotel/hotelFlights";
+import HotelMap from "@/components/hotel/hotelMap";
+import HotelSelector from "@/components/hotel/HotelSelector";
 export default {
+  components:{
+    HotelFlights,
+    HotelSelector,
+    HotelMap
+  }
 
 }
 </script>
 
-<style>
+<style scoped lang="less">
+.container{
+  width: 1000px;
+  margin:10px auto;
+  .el-row{
+    margin-top:20px;
+  }
+}
 
 </style>
