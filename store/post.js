e426fead6fbcac3fcc693d5bf75898c28c29fb1b
lalala <iuyangdaxia@163.com>
2020-02-22 00:11:53 +0800

    发布攻略页面完成

--- store/post.js
+++ store/post.js
@@ -1,5 +1,17 @@
-export const state = () => {
+export const state=()=>{
     return {
-        a: 123
+        //放一个数组存储保存到草稿的数据
+        draft:[]
     }
+}
+
+export const mutations={
+    // 更改state中draft的值
+    setDraft(state,data) {
+        state.draft.unshift(data)
+    },
+    // 删除草稿
+    delDraftData(state, index) {
+        state.draft.splice(index, 1)
+      }
 }
