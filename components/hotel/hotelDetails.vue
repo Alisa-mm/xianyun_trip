a5c86ede885ec15a061ab7edabe752c2d4181ab8
梦幻 <web2018_lang@163.com>
2020-02-23 16:31:20 +0800

    酒店首页详情静态

--- components/hotel/hotelDetails.vue
+++ components/hotel/hotelDetails.vue
@@ -1,5 +1,6 @@
 <template>
   <section class="hotelDetails">
+    <!-- 酒店详情 -->
     <el-row v-for="(item,index) in data" :key="index">
       <el-col :span="6">{{item.photos}}</el-col>
       <el-col :span="12" class="hotelXQ">
@@ -17,29 +18,49 @@
             text-color="#ff9900"
             score-template="{value}"
           ></el-rate>
-          <span>36<i>条评价</i></span>
-          <span>46<i>篇游记</i></span>
+          <span>
+            36
+            <i>条评价</i>
+          </span>
+          <span>
+            46
+            <i>篇游记</i>
+          </span>
         </div>
         <div class="diZhi">
           <i class="el-icon-location"></i>
           <span>{{item.address}}</span>
-          </div>
+        </div>
       </el-col>
       <el-col :span="6">
-        <div>
+        <div class="lianJie">
           <span>携程</span>
-          <span>￥140起 ></span>
+          <span>
+            ￥140起
+            <i class="el-icon-arrow-right"></i>
+          </span>
         </div>
-        <div>
-          携程 ￥
-          <span>140</span>起>
+        <div class="lianJie">
+          <span>携程</span>
+          <span>
+            ￥140起
+            <i class="el-icon-arrow-right"></i>
+          </span>
         </div>
-        <div>
-          携程 ￥
-          <span>140</span>起>
+        <div class="lianJie">
+          <span>携程</span>
+          <span>
+            ￥140起
+            <i class="el-icon-arrow-right"></i>
+          </span>
         </div>
       </el-col>
     </el-row>
+
+    <!-- 分页 -->
+    <el-row class="paging">
+      <el-pagination layout="prev, pager, next" :total="50"></el-pagination>
+      </el-row>
   </section>
 </template>
 
@@ -68,43 +89,65 @@ export default {
 </script>
 
 <style lang="less" scoped>
-.hotelDetails{
-  margin-top:20px;
-  .el-row{
-    margin-bottom:15px;
-    padding-bottom:15px;
-    border-bottom:1px solid #ddd;
-    .hotelXQ{
-      h2{
-        font-size:x-large;
-        font-weight:400;
-        color:inherit;
+.hotelDetails {
+  position: relative;
+  margin-top: 20px;
+  .el-row {
+    margin-bottom: 15px;
+    padding-bottom: 15px;
+    border-bottom: 1px solid #ddd;
+    //酒店详情
+    .hotelXQ {
+      h2 {
+        font-size: x-large;
+        font-weight: 400;
+        color: inherit;
       }
-      .alias{     
-          font-size:16px;
-          margin-bottom:10px;
-          color:#999;
-          .iconfont{
-            color:#ff9900;
-          }
+      .alias {
+        font-size: 16px;
+        margin-bottom: 10px;
+        color: #999;
+        .iconfont {
+          color: #ff9900;
+        }
       }
     }
-    .pingFen{
-      display:flex;
+    .pingFen {
+      display: flex;
       justify-content: flex-start;
-      font-size:16px;
-      color:#ff9900;
-      margin-bottom:10px;
-      span{
-        margin-left:50px;
-        i{
+      font-size: 16px;
+      color: #ff9900;
+      margin-bottom: 10px;
+      span {
+        margin-left: 50px;
+        i {
           color: #000;
         }
       }
     }
-    .diZhi{
-      
+    .diZhi {
+      font-size: 16px;
+      color: #999;
     }
+
+    // 链接
+    .lianJie {
+      display: flex;
+      justify-content: space-between;
+      font-size: 14px;
+      color: #606266;
+      height: 50px;
+      line-height: 50px;
+      padding: 0 30px 0 15px;
+      border-bottom: 1px solid #ddd;
+    }
+  }
+  .paging{
+    // position: absolute;
+    // right:0px;
+    // bottom:0px;
+    margin-left:700px;
+    border:0;
   }
 }
 </style>
